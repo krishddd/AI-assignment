f09784fa8d5f03276d3be747bb6eb26a17b6639e
krishddd <122791972+krishddd@users.noreply.github.com>
2023-02-11 16:17:36 +0530

    Update count_images.py

--- count_images.py
+++ count_images.py
@@ -11,7 +11,7 @@ canny = cv2.Canny(blur, 30, 150, 3)
 dilated = cv2.dilate(canny, (1, 1), iterations=0)
   
 (cnt, hierarchy) = cv2.findContours(
-    dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 cv2.drawContours(rgb, cnt, -1, (0, 255, 0), 2)
   
